96836fa452078030a14103d20a2d636b68128955
Mylon Walker <binkyiv@yahoo.com>
2019-08-19 08:08:16 -0500

    Update home page, added image

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -3,12 +3,19 @@ import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  styleUrls: ['/src/pages/home/home.css']
 })
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
 
   }
+
+  // Optional parameters to pass to the swiper instance. See http://idangero.us/swiper/api/ for valid options.
+  slideOpts = {
+    initialSlide: 1,
+    speed: 400
+  };
 
 }
